4a728cad78f30e782ef982aaa8837c2105c715ff
Prashant <prashantsoni1242@gmail.com>
2023-07-11 00:18:46 +0530

    articles page

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,5 +1,18 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 const Article = () => {
-  return <h1>Article</h1>;
+  const { articleId } = useParams();
+  const article = articles.find((item) => item.name === articleId);
+
+  return (
+    <>
+      <h1>{article.name}</h1>
+      {article.content.map((p, index) => (
+        <p key={index}>{p}</p>
+      ))}
+    </>
+  );
 };
 
 export default Article;
--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,5 +1,25 @@
+import articles from "./article-content";
+import { Link } from "react-router-dom";
+
 const ArticlesList = () => {
-  return <h1>ArticlesList</h1>;
+  return (
+    <>
+      <h1>Articles</h1>
+      {articles.map((item) => (
+        <Link
+          key={item.name}
+          to={`/articles/${item.name}`}
+          style={{
+            color: "black",
+            textDecoration: "none",
+          }}
+        >
+          <h3>{item.title}</h3>
+          <p>{item.content[0].substring(0, 150)}...</p>
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default ArticlesList;
